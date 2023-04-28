eac6ef56775053926facd9a6c8152ffc219fba22
Amraulraul1997@mail.ru <raul1997@mail.ru>
2023-04-28 18:30:48 +0300

    third commit

--- geekshop/views.py
+++ geekshop/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
+from mainapp.models import Product
+
+links_main_menu = [
+        {'href': '/', 'name': 'домой', 'route': ''},
+        {'href': '/products/', 'name': 'продукты','route': 'products/'},
+        {'href': '/contact/', 'name': 'контакты', 'route': 'contact/'},
+    ]
 def index(request):
     title = 'GeekShop'
+    products = Product.objects.all()[:4]
+
     context = {
         'title': title,
+        'links_main_menu': links_main_menu,
+        'products': products
     }
     return render(request, 'geekshop/index.html', context)
 
@@ -11,5 +22,6 @@ def contacts(request):
     title = 'контакты'
     context = {
         'title': title,
+        'links_main_menu': links_main_menu,
     }
     return render(request, 'geekshop/contact.html', context)
